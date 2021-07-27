3a941bdc8d357806c6467f1293eb77eb1e9f8e4a
noxiossy <cari0us@outlook.com>
2021-07-27 15:39:58 +0700

    12

--- src/xrCDB/StdAfx.cpp
+++ src/xrCDB/StdAfx.cpp
@@ -5,16 +5,5 @@
 #include "stdafx.h"
 #pragma hdrstop
 
-#ifdef __BORLANDC__
-	#pragma comment(lib,"xrCoreB.lib")
-#else
-	#pragma comment(lib,"xrCore.lib")
-
-	#pragma comment(lib,"xrApi.lib")
-
-#endif
-
-#pragma comment(lib,"winmm.lib")
-
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
