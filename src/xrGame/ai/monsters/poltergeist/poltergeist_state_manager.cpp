bab1259f3cccba72d9f14da447fa385b228b852a
noxiossy <72839483+noxiossy@users.noreply.github.com>
2021-08-03 17:01:31 +0700

    Update poltergeist_state_manager.cpp

--- src/xrGame/ai/monsters/poltergeist/poltergeist_state_manager.cpp
+++ src/xrGame/ai/monsters/poltergeist/poltergeist_state_manager.cpp
@@ -57,7 +57,7 @@ void CStateManagerPoltergeist::execute()
 
 	const CEntityAlive* enemy	= object->EnemyMan.get_enemy();
 
-	if (enemy  && object->detected_enemy()) {
+	if (enemy /* && object->detected_enemy()*/) {
 		if (object->is_hidden()) state_id = eStateAttack_AttackHidden;
 		else {
 			switch (object->EnemyMan.get_danger_type()) {
@@ -82,7 +82,7 @@ void CStateManagerPoltergeist::execute()
 					object->CEnergyHolder::deactivate();
 				}
 				
-				object->DisableHide();
+				object->Show();
 			}
 		}
 
@@ -91,10 +91,10 @@ void CStateManagerPoltergeist::execute()
 	//if (state_id == eStateAttack_AttackHidden) polter_attack();
 
 	if ((prev_substate == eStateEat) && (state_id != eStateEat) && !(object->conditions().GetHealth() < 0.3f)) 
-		object->EnableHide();
+		object->Hide();
 
 	if (object->conditions().GetHealth() < 0.3f)
-		object->DisableHide();
+		object->Show();
 
 	select_state(state_id); 
 
