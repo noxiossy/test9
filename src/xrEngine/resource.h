25a13cbe1beff328e7ef854d9c281088b890f091
noxiossy <72839483+noxiossy@users.noreply.github.com>
2021-09-19 02:34:37 +0700

    Update resource.h

--- src/xrEngine/resource.h
+++ src/xrEngine/resource.h
@@ -6,6 +6,10 @@
 #define IDI_ICON1   111
 #define IDB_BITMAP2 116
 
+#define IDC_STATIC_LOGO 1007
+#define RC_VIRT_MEM_ERROR 2071
+#define RC_VIRT_MEM_TEXT 2072
+
 #ifndef IDC_STATIC
 #define IDC_STATIC  -1
 #endif
