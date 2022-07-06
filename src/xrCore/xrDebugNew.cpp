0158fe8fde96d550cb8b21919778426b18e7b4f3
noxiossy <cari0us@outlook.com>
2022-07-06 17:48:58 +0700

    dasda

--- src/xrCore/xrDebugNew.cpp
+++ src/xrCore/xrDebugNew.cpp
@@ -383,21 +383,25 @@ void __cdecl xrDebug::fatal(const char* file, int line, const char* function, co
 	backend		("fatal error","<no expression>",buffer,0,file,line,function,ignore_always);
 }
 
+typedef void(*full_memory_stats_callback_type) ();
+XRCORE_API full_memory_stats_callback_type g_full_memory_stats_callback = 0;
+
 int out_of_memory_handler	(size_t size)
 {
-	Memory.mem_compact		();
-#ifndef _EDITOR
-	u32						crt_heap		= mem_usage_impl((HANDLE)_get_heap_handle(),0,0);
-#else // _EDITOR
-	u32						crt_heap		= 0;
-#endif // _EDITOR
-	u32						process_heap	= mem_usage_impl(GetProcessHeap(),0,0);
-	int						eco_strings		= (int)g_pStringContainer->stat_economy			();
-	int						eco_smem		= (int)g_pSharedMemoryContainer->stat_economy	();
-	Msg						("* [x-ray]: crt heap[%d K], process heap[%d K]",crt_heap/1024,process_heap/1024);
-	Msg						("* [x-ray]: economy: strings[%d K], smem[%d K]",eco_strings/1024,eco_smem);
-	Debug.fatal				(DEBUG_INFO,"Out of memory. Memory request: %d K",size/1024);
-	return					1;
+-    if (g_full_memory_stats_callback)
+        g_full_memory_stats_callback();
+    else
+    {
+        Memory.mem_compact();
+        size_t process_heap = Memory.mem_usage();
+        int eco_strings = (int) g_pStringContainer->stat_economy();
+        int eco_smem = (int) g_pSharedMemoryContainer->stat_economy();
+        Msg("* [x-ray]: process heap[%u K]", process_heap / 1024, process_heap / 1024);
+        Msg("* [x-ray]: economy: strings[%d K], smem[%d K]", eco_strings / 1024, eco_smem);
+    }
+
+    Debug.fatal(DEBUG_INFO, "Out of memory. Memory request: %d K", size / 1024);
+    return 1;
 }
 
 extern LPCSTR log_name();
